3a23914ae3fe07ea77f063df0d9f1a9908e3bb65
Jérémie <barriere.jeremie@gmail.com>
2022-12-23 00:04:41 +0100

    [fix] head

--- pages/logout.js
+++ pages/logout.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import { useEffect } from "react"
 import { MobileTemplate, NewTemplate, Template } from "../components/organisms"
 import { useUserContext } from "../context"
