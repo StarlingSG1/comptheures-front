327f79a51c8f2311064b7d42b234ca570ce9b74a
Jérémie <barriere.jeremie@gmail.com>
2022-12-29 14:23:20 +0100

    [edit] logox2

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/logo2.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logox2.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
         <link rel="icon" type="image/png" href="/favicon.png" />
