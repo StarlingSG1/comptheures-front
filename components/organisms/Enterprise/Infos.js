8c6ac3f9e0e3a5a6123acf354151f8e3f353f383
Jérémie <barriere.jeremie@gmail.com>
2023-01-29 12:00:29 +0100

    [add] horaires à valider + selectedUser

--- components/organisms/Enterprise/Infos.js
+++ components/organisms/Enterprise/Infos.js
@@ -14,77 +14,46 @@ export function Infos({ }) {
     const { setBurgerOpen, user, setUser } = useUserContext()
 
     const [edit, setEdit] = useState(true)
-    const [enterprise, setEnterprise] = useState({
-        name: "Maison de la Barbe à Papa",
-        adminName: "John Doe",
-        adress: "1 rue de la barbe à papa",
-        email: "contact@barbeapapa",
-        phone: "06 00 00 00 00",
-        website: "https://comptheures.fr",
-    })
+    const [enterprise, setEnterprise] = useState({})
 
-    const [enterpriseInfo, setEnterpriseInfo] = useState({
-        name: enterprise?.name,
-        adminName: enterprise?.firstName,
-        adress: enterprise?.lastName,
-        email: enterprise?.email,
-        phone: enterprise?.phone,
-        website: enterprise?.website,
-    })
+    const [enterpriseInfo, setEnterpriseInfo] = useState({})
 
     const router = useRouter()
 
-
-
     const validateUpdateUser = async (e) => {
         e.preventDefault()
-        if (enterpriseInfo.firstName === "" || enterpriseInfo.lastName === "" || enterpriseInfo.email === "") {
-            toast.error("Veuillez remplir tous les champs")
-        }
-        else if (enterpriseInfo.password.new !== enterpriseInfo.password.confirm) {
-            toast.error("Les mots de passe ne correspondent pas")
-        }
-        else if (enterpriseInfo.password.old === enterpriseInfo.password.new && enterpriseInfo.password.new === enterpriseInfo.password.confirm && enterpriseInfo.password.old !== "") {
-            toast.error("Le nouveau mot de passe doit être différent de l'ancien")
-        }
-        else {
-            const response = await updateUserProfile(enterpriseInfo)
-            if (response.error === false) {
-                setUser(enterpriseInfo)
-                toast.success(response.message)
-                const newUser = enterpriseInfo
-                setEnterpriseInfo({
-                    firstName: newUser?.firstName,
-                    lastName: newUser?.lastName,
-                    email: newUser?.email,
-                    password: {
-                        old: "",
-                        new: "",
-                        confirm: ""
-                    }
-                })
-            }
-        }
-        (clocks)
     }
-
-
 
     useEffect(() => {
         setBurgerOpen(false);
+        setEnterprise(user?.userEnterprise?.enterprise)
     }, [user])
+
+    useEffect(() => {
+        console.log(enterprise)
+        setEnterpriseInfo(
+            {
+                name: enterprise?.name,
+                adminName: enterprise?.firstName,
+                adress: enterprise?.lastName,
+                email: enterprise?.email,
+                phone: enterprise?.phone,
+                website: enterprise?.website,
+            }
+        )
+    }, [enterprise])
 
     return (
         <form onSubmit={validateUpdateUser} className="">
             <OrbitronTitle className="text-center !font-normal">{enterprise?.name}</OrbitronTitle>
             <BackTitle>Informations de l'entreprise</BackTitle>
-                    <div className="flex flex-col mt-10 gap-[30px] wp-full">
-                        <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, adminName: e.target.value })} defaultValue={enterprise?.adminName} placeholder="Administrateur" />
-                        <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, adress: e.target.value })} defaultValue={enterprise?.adress} placeholder="Adresse" />
-                        <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, email: e.target.value })} defaultValue={enterprise?.email} placeholder="Adresse email" />
-                        <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, phone: e.target.value })} defaultValue={enterprise?.phone} placeholder="Téléphone" />
-                        <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, website: e.target.value })} defaultValue={enterprise?.website} placeholder="Site web" />
-                    </div>
+            <div className="flex flex-col mt-10 gap-[30px] wp-full">
+                <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, adminName: e.target.value })} defaultValue={enterprise?.adminName} placeholder="Administrateur (createdBy userEnterpriseId)" />
+                <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, adress: e.target.value })} defaultValue={enterprise?.adress} placeholder="Adresse" />
+                <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, email: e.target.value })} defaultValue={enterprise?.email} placeholder="Adresse email" />
+                <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, phone: e.target.value })} defaultValue={enterprise?.phone} placeholder="Téléphone" />
+                <OpenInput onChange={(e) => setEnterpriseInfo({ ...enterpriseInfo, website: e.target.value })} defaultValue={enterprise?.website} placeholder="Site web" />
+            </div>
             <Button type="submit" className="mt-60 md:mb-0 mb-60">Enregistrer</Button>
         </form>
     )
--- pages/enterprise/clocks/validation.js
+++ pages/enterprise/clocks/validation.js
@@ -39,6 +39,14 @@ export default function enterpriseValidation() {
     const router = useRouter()
 
     const [usersData, setUsersData] = useState([])
+    const [details, setDetails] = useState(false)
+    const [selectedUser, setSelectedUser] = useState({})
+
+    const handleDetails = (item) => {
+        console.log(item)
+        setDetails(true)
+        setSelectedUser(item)
+    }
 
     return (
         <>
@@ -51,30 +59,64 @@ export default function enterpriseValidation() {
             </Head>
             <NewTemplate>
                 {!user ? <Redirect /> :
-                    <div>
-                        <OrbitronTitle className="text-center !font-normal">{"Maison de la Barbe à Papa"}</OrbitronTitle>
-                        <BackTitle>Horaires à valider</BackTitle>
-                        <table className="w-full border border-blue dark:border-white border-2">
-                            <thead className="w-full text-left bg-blue dark:bg-blue-dark text-white">
-                                <tr className="h-10">
-                                    <th className="pl-2.5">Nom</th>
-                                    <th className="pl-2.5">Prénom</th>
-                                    <th className="pl-2.5">Rôle</th>
-                                    <th className="pl-2.5">Horaires à valider</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {usersData?.map((item, index) => (
-                                    <tr key={index} className="dark:even:bg-blue-dark even:bg-blue odd:bg-transparent even:text-white dark:odd:text-white h-10 ">
-                                        <td className="pl-2.5">{item.user.lastName}</td>
-                                        <td className="pl-2.5">{item.user.firstName}</td>
-                                        <td className="pl-2.5">{item.role.label}</td>
-                                        <td className="pl-2.5">{item.total}</td> 
+                    !details ?
+                        <div>
+                            <OrbitronTitle className="text-center !font-normal">{"Maison de la Barbe à Papa"}</OrbitronTitle>
+                            <BackTitle>Horaires à valider</BackTitle>
+                            <table className="w-full border border-blue dark:border-white border-2">
+                                <thead className="w-full text-left bg-blue dark:bg-blue-dark text-white">
+                                    <tr className="h-10">
+                                        <th className="pl-2.5">Nom</th>
+                                        <th className="pl-2.5">Prénom</th>
+                                        <th className="pl-2.5">Rôle</th>
+                                        <th className="pl-2.5">Horaires à valider</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+                                </thead>
+                                <tbody>
+                                    {usersData?.map((item, index) => (
+                                        item.total > 0 &&
+                                        <tr key={index} onClick={() => handleDetails(item)} className="cursor-pointer dark:hover:bg-blue-selected hover:bg-blue-selected hover:text-white dark:hover:text-white dark:even:bg-blue-dark even:bg-blue odd:bg-transparent even:text-white dark:odd:text-white h-10 ">
+                                            <td className="pl-2.5">{item.user.lastName}</td>
+                                            <td className="pl-2.5">{item.user.firstName}</td>
+                                            <td className="pl-2.5">{item.role.label}</td>
+                                            <td className="pl-2.5">{item.total}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        :
+                        <div>
+                            <OrbitronTitle className="text-center !font-normal">{"Maison de la Barbe à Papa"}</OrbitronTitle>
+                            <div className="flex items-center justify-between mt-[25px] mb-[50px]">
+                                <RealArrow onClick={() => setDetails(false)} width={40} height={40} className="cursor-pointer rotate-180" />
+                                <SubTitle className="text-center ">{selectedUser?.user?.firstName}{" "}{selectedUser?.user?.lastName}</SubTitle>
+                                <RealArrow width={40} height={40} className="invisible" />
+                            </div>
+                            <table className="w-full border border-blue dark:border-white border-2">
+                                <thead className="w-full text-left bg-blue dark:bg-blue-dark text-white">
+                                    <tr className="h-10">
+                                        <th className="pl-2.5">Date</th>
+                                        <th className="pl-2.5">Nombre d'heures</th>
+                                        <th className="pl-2.5">Type de journée</th>
+                                        <th className="pl-2.5">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {selectedUser?.Stats?.map((item, index) => (
+                                        <tr key={index} className="dark:hover:text-white dark:even:bg-blue-dark even:bg-blue odd:bg-transparent even:text-white dark:odd:text-white h-10 ">
+                                            {item.realisationStatus === "IN_VALIDATION" &&
+                                                <>
+                                                    <td className="pl-2.5">{item.day}</td>
+                                                    <td className="pl-2.5">{item.work}</td>
+                                                    <td className="pl-2.5">{item?.CustomTime?.length > 0 ? "Personnalisé" : "Special"}</td>
+                                                    <td className="pl-2.5">{item.week}</td>
+                                                </>}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                 }
             </NewTemplate>
         </>
