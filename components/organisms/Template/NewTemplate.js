d6735001f54603be7b2ba3ac6ff0fb50d8f73384
Jérémie <barriere.jeremie@gmail.com>
2023-02-04 17:08:44 +0100

    [edit] card appear only on comptheures page

--- components/organisms/Template/NewTemplate.js
+++ components/organisms/Template/NewTemplate.js
@@ -13,54 +13,59 @@ export function NewTemplate({ children, comptheures = false, className = "" }) {
     const { user } = useUserContext()
     const [todayTime, setTodayTime] = useState(null)
     const [todayStatus, setTodayStatus] = useState(null)
+
+    const isComptheures = () => {
+
+    }
+
     const checkTimeToday = () => {
         const today = times.find(time => time.day === currentDay.getDate() && time.month === currentDay.getMonth() && time.year === currentDay.getFullYear())
         if (today?.realisationStatus) {
             switch (today.realisationStatus) {
                 case "IN_VALIDATION":
-                   setTodayStatus("En attente de validation")
+                    setTodayStatus("En attente de validation")
                     break;
                 case "VALIDATED":
-                   setTodayStatus("Validé par l'administateur")
+                    setTodayStatus("Validé par l'administateur")
                     break;
                 case "REFUSED":
-                   setTodayStatus("Refusé par l'administateur")
+                    setTodayStatus("Refusé par l'administateur")
                     break;
                 default:
-            setTodayTime(today)
+                    setTodayTime(today)
             }
-        }else{
+        } else {
             setTodayTime(null)
             setTodayStatus(null)
         }
     }
 
-        useEffect(() => {
-            checkTimeToday()
-        }, [times, currentDay])
+    useEffect(() => {
+        checkTimeToday()
+    }, [times, currentDay])
 
-        return (
-            <>
-                <div className="md:py-[150px] md:flex md:dark:bg-blue-dark md:bg-blue md:w-screen md:justify-center md:items-center md:min-h-screen">
-                    <div className="md:w-full md:max-w-[1030px] md:grid md:grid-cols-12 md:gap-[50px] md:items-start md:gap-[50px]">
-                        <CrossIcon />
-                        <MobileBurger className="md:hidden" />
-                        <div className={`w-screen py-10 flex flex-col justify-between px-[5%] min-h-screen dark:bg-blue bg-white relative   md:w-auto md:block md:col-span-8 md:min-h-[520px] md:h-full md:shadow md:px-[30px] md:rounded-2xl`}>
-                            {children}
-                            <Footer className="md:hidden" />
-                        </div>
-
-                        {true ?
-                            <div className="hidden col-span-4 md:flex gap-10 flex-col">
-                                <SmallDesktopCard />
-                                <div className="shadow dark:bg-blue bg-white rounded-2xl py-5 px-[15px] flex flex-col items-center gap-[15px]" >
-                                    <Paragraph className="text-center"><strong>Automatique : </strong>{user?.userEnterprise?.enterprise?.configEnterprise?.workHourADay}</Paragraph>
-                                    {todayStatus && <Paragraph className="text-center"><strong>Statut : </strong>{todayStatus}</Paragraph>}
-                                </div>
-                            </div>
-                            : <SmallDesktopCard />}
+    return (
+        <>
+            <div className="md:py-[150px] md:flex md:dark:bg-blue-dark md:bg-blue md:w-screen md:justify-center md:items-center md:min-h-screen">
+                <div className="md:w-full md:max-w-[1030px] md:grid md:grid-cols-12 md:gap-[50px] md:items-start md:gap-[50px]">
+                    <CrossIcon />
+                    <MobileBurger className="md:hidden" />
+                    <div className={`w-screen py-10 flex flex-col justify-between px-[5%] min-h-screen dark:bg-blue bg-white relative   md:w-auto md:block md:col-span-8 md:min-h-[520px] md:h-full md:shadow md:px-[30px] md:rounded-2xl`}>
+                        {children}
+                        <Footer className="md:hidden" />
                     </div>
+                    {comptheures
+                        ?
+                        <div className="hidden col-span-4 md:flex gap-10 flex-col">
+                            <SmallDesktopCard />
+                            <div className="shadow dark:bg-blue bg-white rounded-2xl py-5 px-[15px] flex flex-col items-center gap-[15px]" >
+                                <Paragraph className="text-center"><strong>Automatique : </strong>{user?.userEnterprise?.enterprise?.configEnterprise?.workHourADay}</Paragraph>
+                                {todayStatus && <Paragraph className="text-center"><strong>Statut : </strong>{todayStatus}</Paragraph>}
+                            </div>
+                        </div>
+                        : <SmallDesktopCard />}
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
+}
